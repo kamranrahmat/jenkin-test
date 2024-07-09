7fafa3aa80a6ed0b809a615776f6a9bfcbbe1711
kamranrahmat <kamranrahmatullah33@gmail.com>
2024-07-09 17:56:32 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,11 @@ public class Main{
 
     public static void main(String args[]){
         System.out.println("hellow I am from java run by jenkins");
+
+        System.out.println("This is second line");
+
+        for(int i=1;i<=10;i++){
+            System.out.println("kamran"+i);
+        }
     }
 }
